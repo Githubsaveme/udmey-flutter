b743d83390fd81c7b4e4ae18dbc8b968e4d91ba5
Vishal-33333 <vishal9417719812@gmail.com>
2022-02-13 18:12:56 +0530

    magic_8_ball (completed)

--- lib/Magic_Ball/magic_ball_8.dart
+++ lib/Magic_Ball/magic_ball_8.dart
@@ -1,13 +1,49 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Magic Ball 8'),
+          title: Text('ASK ME ANYTHING'),
+          shadowColor: Colors.red,
+          centerTitle: true,
         ),
+        body: const MagicBall(),
       ),
     ),
   );
+}
+
+class MagicBall extends StatefulWidget {
+  const MagicBall({Key? key}) : super(key: key);
+  @override
+  _MagicBallState createState() => _MagicBallState();
+}
+
+class _MagicBallState extends State<MagicBall> {
+  int ballNumber = 5;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        // ignore: deprecated_member_use
+        FlatButton(
+          onPressed: () {setState(() {
+            ballNumber=Random().nextInt(5)+1;
+            print('BallNumber$ballNumber');
+          });
+
+          },
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Image.asset('images/ball$ballNumber.png'),
+          ),
+        )
+      ],
+    );
+  }
 }
