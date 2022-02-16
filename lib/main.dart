2aaf485d17670a34e9bef110b8a7caa254f86000
Vishal-33333 <vishal9417719812@gmail.com>
2022-02-16 11:54:02 +0530

    Quizzler App(Add False Button  )

--- lib/main.dart
+++ lib/main.dart
@@ -111,6 +111,14 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked false.
+                setState(() {
+                  scoreKeeper.add(
+                    Icon(
+                      Icons.close,
+                      color: Colors.red,
+                    ),
+                  );
+                });
               },
             ),
           ),
