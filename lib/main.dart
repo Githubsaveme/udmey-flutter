d61483de34b783e937bc289a980af49949ac55d3
Vishal-33333 <vishal9417719812@gmail.com>
2022-02-17 20:22:45 +0530

    Add background image

--- lib/main.dart
+++ lib/main.dart
@@ -24,6 +24,10 @@ class _StoryPageState extends State<StoryPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
+        decoration: BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage('images/background.png'), fit: BoxFit.cover)),
+
         //TODO: Step 1 - Add background.png to this Container as a background image.
         padding: EdgeInsets.symmetric(vertical: 50.0, horizontal: 15.0),
         constraints: BoxConstraints.expand(),
