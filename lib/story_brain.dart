a988e540bafbb94e4cd51b782a8f63fb8991f1e3
Vishal-33333 <vishal9417719812@gmail.com>
2022-02-17 22:22:33 +0530

    FlatButton Wrap with Visibility Widget

--- lib/story_brain.dart
+++ lib/story_brain.dart
@@ -1,15 +1,14 @@
 //TODO: Step 6 - import the story.dart file into this file.
 import 'story.dart';
+
 //TODO: Step 5 - Create a new class called StoryBrain.
 class StoryBrain {
-
-
 //TODO: Step 7 - Uncomment the lines below to include storyData as a private property in StoryBrain. Hint: You might need to change something in story.dart to make this work.
 
   List<Story> _storyData = [
     Story(
         storyTitle:
-        'Your car has blown a tire on a winding road in the middle of nowhere with no cell phone reception. You decide to hitchhike. A rusty pickup truck rumbles to a stop next to you. A man with a wide brimmed hat with soulless eyes opens the passenger door for you and asks: "Need a ride, boy?".',
+            'Your car has blown a tire on a winding road in the middle of nowhere with no cell phone reception. You decide to hitchhike. A rusty pickup truck rumbles to a stop next to you. A man with a wide brimmed hat with soulless eyes opens the passenger door for you and asks: "Need a ride, boy?".',
         choice1: 'I\'ll hop in. Thanks for the help!',
         choice2: 'Better ask him if he\'s a murderer first.'),
     Story(
@@ -18,22 +17,22 @@ class StoryBrain {
         choice2: 'Wait, I know how to change a tire.'),
     Story(
         storyTitle:
-        'As you begin to drive, the stranger starts talking about his relationship with his mother. He gets angrier and angrier by the minute. He asks you to open the glovebox. Inside you find a bloody knife, two severed fingers, and a cassette tape of Elton John. He reaches for the glove box.',
+            'As you begin to drive, the stranger starts talking about his relationship with his mother. He gets angrier and angrier by the minute. He asks you to open the glovebox. Inside you find a bloody knife, two severed fingers, and a cassette tape of Elton John. He reaches for the glove box.',
         choice1: 'I love Elton John! Hand him the cassette tape.',
         choice2: 'It\'s him or me! You take the knife and stab him.'),
     Story(
         storyTitle:
-        'What? Such a cop out! Did you know traffic accidents are the second leading cause of accidental death for most adult age groups?',
+            'What? Such a cop out! Did you know traffic accidents are the second leading cause of accidental death for most adult age groups?',
         choice1: 'Restart',
         choice2: ''),
     Story(
         storyTitle:
-        'As you smash through the guardrail and careen towards the jagged rocks below you reflect on the dubious wisdom of stabbing someone while they are driving a car you are in.',
+            'As you smash through the guardrail and careen towards the jagged rocks below you reflect on the dubious wisdom of stabbing someone while they are driving a car you are in.',
         choice1: 'Restart',
         choice2: ''),
     Story(
         storyTitle:
-        'You bond with the murderer while crooning verses of "Can you feel the love tonight". He drops you off at the next town. Before you go he asks you if you know any good places to dump bodies. You reply: "Try the pier".',
+            'You bond with the murderer while crooning verses of "Can you feel the love tonight". He drops you off at the next town. Before you go he asks you if you know any good places to dump bodies. You reply: "Try the pier".',
         choice1: 'Restart',
         choice2: '')
   ];
@@ -55,12 +54,13 @@ class StoryBrain {
     return _storyData[_storyNumber].choice2;
   }
 }
+
 //TODO: Step 25 - Change the storyNumber property into a private property so that only story_brain.dart has access to it. You can do this by right clicking on the name (storyNumber) and selecting Refactor -> Rename to make the change across all the places where it's used.
 //TODO: Step 16 - Create a property called storyNumber which starts with a value of 0. This will be used to track which story the user is currently viewing.
-int _storyNumber=0;
+int _storyNumber = 0;
 
 //TODO: Step 17 - Create a method called nextStory(), it should not have any outputs but it should have 1 input called choiceNumber which will be the choice number (int) made by the user.
-void nextStory(int userChoice){
+void nextStory(int userChoice) {
   if (userChoice == 1 && _storyNumber == 0) {
     _storyNumber = 2;
   } else if (userChoice == 2 && _storyNumber == 0) {
@@ -73,12 +73,14 @@ void nextStory(int userChoice){
     _storyNumber = 5;
   } else if (userChoice == 2 && _storyNumber == 2) {
     _storyNumber = 4;
-  }
-  else if (_storyNumber == 3 || _storyNumber == 4 || _storyNumber == 5) {
+  } else if (_storyNumber == 3 || _storyNumber == 4 || _storyNumber == 5) {
     restart();
   }
 }
-void restart(){_storyNumber=0;}
+
+void restart() {
+  _storyNumber = 0;
+}
 //TODO: Step 20 - Download the story plan here: https://drive.google.com/uc?export=download&id=1KU6EghkO9Hf2hRM0756xFHgNaZyGCou3
 
 //TODO: Step 21 - Using the story plan, update nextStory() to change the storyNumber depending on the choice made by the user. e.g. if choiceNumber was equal to 1 and the storyNumber is 0, the storyNumber should become 2.
