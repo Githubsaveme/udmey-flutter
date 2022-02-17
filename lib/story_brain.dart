586ea90672e60981061271ed259609646eddecad
Vishal-33333 <vishal9417719812@gmail.com>
2022-02-17 22:18:56 +0530

    Refactor storyNumber to _storyNumber

--- lib/story_brain.dart
+++ lib/story_brain.dart
@@ -42,43 +42,43 @@ class StoryBrain {
 
 //TODO: Step 8 - Create a method called getStory() that returns the first storyTitle from _storyData.
   String getStory() {
-    return _storyData[storyNumber].storyTitle;
+    return _storyData[_storyNumber].storyTitle;
   }
 
 //TODO: Step 11 - Create a method called getChoice1() that returns the text for the first choice1 from _storyData.
   String getChoice1() {
-    return _storyData[storyNumber].choice1;
+    return _storyData[_storyNumber].choice1;
   }
 
 //TODO: Step 12 - Create a method called getChoice2() that returns the text for the first choice2 from _storyData.
   String getChoice2() {
-    return _storyData[storyNumber].choice2;
+    return _storyData[_storyNumber].choice2;
   }
 }
 //TODO: Step 25 - Change the storyNumber property into a private property so that only story_brain.dart has access to it. You can do this by right clicking on the name (storyNumber) and selecting Refactor -> Rename to make the change across all the places where it's used.
 //TODO: Step 16 - Create a property called storyNumber which starts with a value of 0. This will be used to track which story the user is currently viewing.
-int storyNumber=0;
+int _storyNumber=0;
 
 //TODO: Step 17 - Create a method called nextStory(), it should not have any outputs but it should have 1 input called choiceNumber which will be the choice number (int) made by the user.
 void nextStory(int userChoice){
-  if (userChoice == 1 && storyNumber == 0) {
-    storyNumber = 2;
-  } else if (userChoice == 2 && storyNumber == 0) {
-    storyNumber = 1;
-  } else if (userChoice == 1 && storyNumber == 1) {
-    storyNumber = 2;
-  } else if (userChoice == 2 && storyNumber == 1) {
-    storyNumber = 3;
-  } else if (userChoice == 1 && storyNumber == 2) {
-    storyNumber = 5;
-  } else if (userChoice == 2 && storyNumber == 2) {
-    storyNumber = 4;
+  if (userChoice == 1 && _storyNumber == 0) {
+    _storyNumber = 2;
+  } else if (userChoice == 2 && _storyNumber == 0) {
+    _storyNumber = 1;
+  } else if (userChoice == 1 && _storyNumber == 1) {
+    _storyNumber = 2;
+  } else if (userChoice == 2 && _storyNumber == 1) {
+    _storyNumber = 3;
+  } else if (userChoice == 1 && _storyNumber == 2) {
+    _storyNumber = 5;
+  } else if (userChoice == 2 && _storyNumber == 2) {
+    _storyNumber = 4;
   }
-  else if (storyNumber == 3 || storyNumber == 4 || storyNumber == 5) {
+  else if (_storyNumber == 3 || _storyNumber == 4 || _storyNumber == 5) {
     restart();
   }
 }
-void restart(){}
+void restart(){_storyNumber=0;}
 //TODO: Step 20 - Download the story plan here: https://drive.google.com/uc?export=download&id=1KU6EghkO9Hf2hRM0756xFHgNaZyGCou3
 
 //TODO: Step 21 - Using the story plan, update nextStory() to change the storyNumber depending on the choice made by the user. e.g. if choiceNumber was equal to 1 and the storyNumber is 0, the storyNumber should become 2.
