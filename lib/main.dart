9c0139cc28539077401e561a7edf47026e38c9d1
Vishal-33333 <vishal9417719812@gmail.com>
2022-02-18 13:26:02 +0530

    Bmi (add Container Box  )

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:ui';
 
 void main() => runApp(BMICalculator());
 
@@ -6,6 +7,11 @@ class BMICalculator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        primaryColor: Color(0xFF0A0D22),
+        scaffoldBackgroundColor: Color(0xFF0A0D22),
+        accentColor: Colors.indigoAccent,
+      ),
       home: InputPage(),
     );
   }
@@ -23,8 +29,12 @@ class _InputPageState extends State<InputPage> {
       appBar: AppBar(
         title: Text('BMI CALCULATOR'),
       ),
-      body: Center(
-        child: Text('Body Text'),
+      body: Container(
+        margin: EdgeInsets.all(15.0),
+        decoration: BoxDecoration(
+          color: Color(0xFF1D1F33),
+          borderRadius: BorderRadius.circular(10.0),
+        ),height: 200,width: 170,
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
