1ba12e88d72787c12f92353c657bb37020eaeb30
Vishal-33333 <vishal9417719812@gmail.com>
2022-02-19 15:09:08 +0530

    Bmi (final  )

--- lib/main.dart
+++ lib/main.dart
@@ -87,13 +87,13 @@ class _InputPageState extends State<InputPage> {
 }
 
 class ReusableCard extends StatelessWidget {
-  ReusableCard({@required this.colour});
+  ReusableCard({@required this.colour, @required this.cardChild});
 
   final Color colour;
-
+final Widget cardChild;
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return Container(child: cardChild,
       margin: EdgeInsets.all(15.0),
       decoration: BoxDecoration(
         color: Color(0xFF1D1F33),
