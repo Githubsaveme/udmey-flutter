7a9e1f092111228721b435c0e5bd64ebd6d7e387
Vishal-33333 <vishal9417719812@gmail.com>
2022-02-19 16:12:34 +0530

    BMI (Challenge Extract Widget(IconContent))

--- lib/main.dart
+++ lib/main.dart
@@ -85,25 +85,33 @@ class _InputPageState extends State<InputPage> {
       ),
     );
   }
+}
 
-  Column IconContent() {
-    return Column(mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Icon(
-                      FontAwesomeIcons.mars,
-                      color: Colors.white,
-                      size: 80.0,
-                    ),
-                    SizedBox(
-                      height: 15.0,
-                    ),
-                    Text(
-                      'MALE',
-                      style:
-                          TextStyle(color: Color(0xFF8D8E98), fontSize: 12),
-                    ),
-                  ],
-                );
+class IconContent extends StatelessWidget {
+  const IconContent({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(
+          FontAwesomeIcons.mars,
+          color: Colors.white,
+          size: 80.0,
+        ),
+        SizedBox(
+          height: 15.0,
+        ),
+        Text(
+          'MALE',
+          style:
+              TextStyle(color: Color(0xFF8D8E98), fontSize: 18),
+        ),
+      ]
+    );
   }
 }
 
